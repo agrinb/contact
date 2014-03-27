a29eb63490aa9b881007ad7713911ee0b3d9959c
Alek <agrinb@gmail.com>
2014-03-27 18:45:01 -0400

    Second Push/Bootstrap added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contact::Application.config.secret_key_base = '7c23b9c35c9843968244f7e6b8e6c1be87208cc71f2800d325522c45303a8b491fef4843dc1d109095c0333535101a858f072a801ab9df585ce0582237353ecd'
